074ca261332a03ed23ec4708f39d749bdadbc3ae
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-01-28 18:59:23 +0530

    navnar path no isuue found

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Hero />} />
-        <Route path="/b" element={<Footer />} />
+        <Route path="/n" element={<Navbar />} />
       </Routes>
     </>
   );
